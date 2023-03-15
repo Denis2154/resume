694d98acf95217bd0ddad4437d55a0a466fca640
Denis2154 <denalekc92@gmail.com>
2023-03-15 16:36:56 +0200

    basic

--- src/route/index.js
+++ src/route/index.js
@@ -271,7 +271,7 @@ router.get('/work', function (req, res) {
 router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('bio', {
-    layout: 'bio',
+    layout: 'basic',
     name: 'Albert Einstein',
     birthdate: 'March 14, 1879',
     birthplace:
@@ -366,7 +366,7 @@ router.get('/bio', function (req, res) {
 router.get('/person', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('person', {
-    layout: 'person',
+    layout: 'basic',
 
     person: {
       name: 'Emma Johnson',
