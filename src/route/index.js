98892e38d9bad0ac49099ed82b7bc25bb77ec820
Denis2154 <denalekc92@gmail.com>
2023-03-19 18:37:00 +0200

    task21

--- src/route/index.js
+++ src/route/index.js
@@ -489,6 +489,7 @@ router.get('/task21', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('task21', {
+    layout: 'basic',
     heading: {
       main: {
         title: 'JavaScript',
